326d80ddf1f85567470e20574d63881973540d68
DIVYA-NIMKARDE <divyanimkarde@gmail.com>
2024-06-24 13:01:36 +0530

    added multiply.java

---
+++ multiply.java
@@ -0,0 +1,13 @@
+ 
+
+public class multiply {
+    public static void main(String[] args) {
+        
+        System.out.println("multiply new");
+    }
+
+    // Intentional Checkstyle violations:
+    // 1. Missing Javadoc comment for the class
+    // 2. Missing Javadoc comment for the main method
+    // 3. System.out.println should be avoided in favor of a logger
+}
